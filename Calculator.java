cb113e16b41cb2e54ad89955d6204289b6a15ef1
ahlo <lo.drew24@gmail.com>
2015-04-04 13:39:11 -0400

    testing

---
+++ Calculator.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+import java.awt.*;
+
+public class Calculator() {
+
+
+
+
+
+#Operations
+static double addition(double number1, double number2) {
+return number1+number2;
+}
+}
