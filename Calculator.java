3ace13c3b89e03275bb77c1ae097fef9931a384c
ahlo <lo.drew24@gmail.com>
2015-04-04 14:28:24 -0400

    Calculator.java format restructed

--- Calculator.java
+++ Calculator.java
@@ -1,14 +1,30 @@
 import java.util.Scanner;
 import java.awt.*;
 
-public class Calculator() {
+public class Calculator {
+    public Calculator(){
+        
+    }
+    
+    static void main(String[] args) {
+        
+    }
 
+    //Basic Arithmetic Operations
+    static double addition(double number1, double number2) {
+        return number1 + number2;
+    }
+    
+    static double subtraction(double number1, double number2) {
+        return number1 - number2;
+    }
+    
+    static double multiplication(double number1, double number2) {
+        return number1 * number2;
+    }
+    
+    static double division(double number1, double number2) {
+        return number1 / number2;
+    }
 
-
-
-
-#Operations
-static double addition(double number1, double number2) {
-return number1+number2;
-}
 }
